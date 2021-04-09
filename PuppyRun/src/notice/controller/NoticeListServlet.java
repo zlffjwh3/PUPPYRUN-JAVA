41f7769f57549613a71742f23df463d21d809b02
BinnieKim <dvk0217@naver.com>
2021-04-09 16:38:19 +0900

    봐바

--- PuppyRun/src/notice/controller/NoticeListServlet.java
+++ PuppyRun/src/notice/controller/NoticeListServlet.java
@@ -1,11 +1,17 @@
 package notice.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import notice.model.service.NoticeService;
+import notice.model.vo.Notice;
+import notice.model.vo.NoticePage;
 
 @WebServlet("/notice/list")
 public class NoticeListServlet extends HttpServlet {
@@ -16,6 +22,34 @@ public class NoticeListServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		HttpSession session = request.getSession();
+//		if(session != null && (session.getAttribute("userId")) != null) {
+		
+		// 관리자인지 확인하는 작업 필요
+		
+			int currentPage = 0;
+			if(request.getParameter("currentPage") == null) {
+				currentPage = 1;
+			} else {
+				currentPage = Integer.parseInt(request.getParameter("currentPage"));
+			}
+			
+			NoticePage pd = new NoticeService().selectAllNotice(currentPage);
+			ArrayList<Notice> nList = pd.getnList();
+			String pageNavi = pd.getPageNavi();
+			
+			if(!nList.isEmpty()) {
+				request.setAttribute("nList", nList);
+				request.setAttribute("pageNavi", pageNavi);
+				request.getRequestDispatcher("/WEB-INF/views/notice/notice.jsp").forward(request, response);
+			} else {
+				request.getRequestDispatcher("/WEB-INF/views/notice/noticeError.html").forward(request, response);
+			}
+
+//		} else {
+//			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/notice/serviceFailed.html");
+//			view.forward(request, response);
+//		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- PuppyRun/src/petdiary/model/service/GoalService.java
+++ PuppyRun/src/petdiary/model/service/GoalService.java
@@ -13,29 +13,29 @@ public class GoalService {
 		factory = JDBCTemplate.getConnection();
 	}
 	
-	// 목표 설정하면 저장하는 메소드
+	// 목표 설정하면 저장하는 메소드 - GoalWriteServlet
 	public int saveUserGoal(Goal goal) {
 		int result = 0;
 		return result;
 	}
 	
-	// 기록 가져오는 메소드
+	// 오늘 기록 가져오는 메소드 - PetDiaryWriteServlet / PetDiaryUpdateServlet
 	public Goal weekGoal(String goalId) {
 		return null;
 	}
 	
-	// 기록 저장하는 메소드
+	// 기록 저장하는 메소드 - PetDiaryWriteServlet / PetDiaryUpdateServlet
 	public int saveTodayGoal(String goalId, Goal goal) {
 		int result = 0;
 		return result;
 	}
 	
-	// 오늘 전체 기록 불러오는 메소드
+	// 전체 기록 불러오는 메소드 - GoalStampServlet
 	public ArrayList<Goal> goalList(String goalId) {
 		return null;
 	}
 	
-	// 아이디별로 목표 달성 몇개했는지 카운트해서 가져오는 메소드
+	// 아이디별로 목표 달성 몇개했는지 카운트해서 가져오는 메소드 - GoalStampServlet
 	public int countGoal(String goalId) {
 		return 0;
 	}
