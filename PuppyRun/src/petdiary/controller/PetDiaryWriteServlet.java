66335fdbba0681b20331428c002646b1daee7df5
OHchan <zlffjwh3@naver.com>
2021-04-13 12:50:02 +0900

    지도지도 다빈이 천재~

--- PuppyRun/src/petdiary/controller/PetDiaryWriteServlet.java
+++ PuppyRun/src/petdiary/controller/PetDiaryWriteServlet.java
@@ -1,40 +1,43 @@
 package petdiary.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class NoticeWriteServlet
- */
-@WebServlet("/calendar/write")
+import petdiary.model.service.PetDiaryService;
+import petdiary.model.vo.PetDiary;
+import user.model.vo.User;
+
+@WebServlet("/petdiary/write")
 public class PetDiaryWriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public PetDiaryWriteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		User user = (User)session.getAttribute("user");
+		String userId = user.getUserId();
+		
+		ArrayList<PetDiary> pList = new PetDiaryService().selectAllDiary(userId);
+		
+		if(!pList.isEmpty()) {
+			request.setAttribute("pList", pList);
+			request.getRequestDispatcher("/WEB-INF/views/pet-diary/diaryWrite.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/WEB-INF/views/pet-diary/diaryError.html").forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
