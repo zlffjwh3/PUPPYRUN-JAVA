cbfc3acf71b3b67ba6a850a0559ead699e60a310
Joo-YeonL <81937349+Joo-YeonL@users.noreply.github.com>
2021-04-15 16:18:52 +0900

    산책짝꿍CSS 수정 + 아이디비밀번호 찾기 1차수정(css수정)

--- PuppyRun/WebContent/assets/js/id-pw-inquiry.js
+++ PuppyRun/WebContent/assets/js/id-pw-inquiry.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     // $("div").css("color", "red");
     // $("#wrap").css('background','pink');
@@ -17,5 +18,7 @@ $(document).ready(function(){
         findId.css('background-color','#F0A300');
         findPwd.css('background-color','#FEB415');
     });
-
+	
 });
+
+// 로그인페이지로 이동하는 펑션만들어서 링크달기
