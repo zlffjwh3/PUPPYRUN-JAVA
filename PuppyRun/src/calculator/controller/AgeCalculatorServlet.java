9895a20a30df1b95079636cabe3a6bbf81e28895
CHANHEE0621 <cksgml0621@naver.com>
2021-04-18 17:14:07 +0900

    호ㅓㅎ

--- PuppyRun/src/calculator/controller/AgeCalculatorServlet.java
+++ PuppyRun/src/calculator/controller/AgeCalculatorServlet.java
@@ -12,24 +12,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AgeCalculatorServler
- */
+
 @WebServlet("/calculator/age")
 public class AgeCalculatorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public AgeCalculatorServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String calc_type = request.getParameter("calc_type");
 		if("2".equals(calc_type)) {
@@ -40,11 +33,8 @@ public class AgeCalculatorServlet extends HttpServlet {
 			request.getRequestDispatcher("/WEB-INF/views/calculator/calculatorAge.jsp").forward(request, response);
 		}
 	}
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
